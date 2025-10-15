e8454980db807390829813e1a5f7ff59c3e0f97d
Abdixoliqov <abdixoliqovhamidullo@gmail.com>
2025-10-15 18:02:53 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="mt-1 text-2xl font-bold">
+        E'lon uz
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+   server: {
+    host: '10.0.1.36', // IP manzilni o'zingizning tarmoq IP'siga moslang
+    port: 5173, // Portni istalgan raqamga o'zgartirishingiz mumkin
+    strictPort: true, // Port band bo'lsa, boshqa portga o'tmasligi uchun
+    open: false, // Avtomatik brauzerda ochmasligi uchun
+  },
+})
